93feb69cc485b7e045560d0acc1dcc06bab3af1d
isx36579183 <isx36579183@i11.informatica.escoladeltreball.org>
2021-10-21 13:47:04 +0200

    21.10.21 - Modificat w Acl

--- ldap21:acl/startup.sh
+++ ldap21:acl/startup.sh
@@ -7,11 +7,10 @@ rm -rf /etc/ldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
 slapadd  -F /etc/ldap/slapd.d -l /opt/docker/edt.org.ldif
-slapcat
 
 chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
 cp /opt/docker/ldap.conf /etc/ldap/ldap.conf
-/usr/sbin/slapd -d0
+/usr/sbin/slapd
 #/usr/sbin/slapd
 
 # Pendent:
