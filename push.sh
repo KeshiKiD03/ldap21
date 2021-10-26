8d8989d40271488b94315c57e8c6aaf497902342
Your Name <you@example.com>
2021-10-27 01:02:00 +0200

    27.10.21 - Editat README

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-git push https://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@github.com/KeshiKiD03/ldap21.git
+git push https://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@github.com/KeshiKiD03/ldap21.git
